3769655d24278ebe9951af1559c11a58878c2123
Emanuele Papagno <emanuelelele92@gmail.com>
2018-06-08 17:18:34 +0200

    Scena MainMenu con bottoni Play e Quit funzionanti

---
+++ Assets/Script/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void loadlevel()
+    {
+       
+            SceneManager.LoadScene("Scene 2 - Copia");
+       
+    }
+}
