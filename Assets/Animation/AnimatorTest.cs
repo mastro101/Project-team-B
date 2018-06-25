13776b27281bd2924421e44583c326e2a6795664
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2018-06-25 19:55:29 +0200

    Creazione Animator Controller dei personaggi

---
+++ Assets/Animation/AnimatorTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorTest : MonoBehaviour {
+
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Walk");
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetTrigger("Attack");
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetTrigger("Death");
+        }
+    }
+}
